0ba639baa8d54e7f1eb405e74dbe02a9085350ec
Maxime Pertici <maxime@whodunit.fr>
2021-09-28 23:59:52 +0200

    item order

--- inc/item-types.php
+++ inc/item-types.php
@@ -4,10 +4,10 @@ defined( 'ABSPATH' ) or	die();
 
 $menu_item_types = array();
 
-$menu_item_types[ 'heading' ]           = array( 'slug' => 'heading',            'label' => __( 'Heading' ,          'menu-item-types') );
-$menu_item_types[ 'image' ]             = array( 'slug' => 'image',              'label' => __( 'Image' ,            'menu-item-types') );
 $menu_item_types[ 'nolink' ]            = array( 'slug' => 'nolink',             'label' => __( 'No link' ,          'menu-item-types') );
+$menu_item_types[ 'heading' ]           = array( 'slug' => 'heading',            'label' => __( 'Heading' ,          'menu-item-types') );
 $menu_item_types[ 'paragraph' ]         = array( 'slug' => 'paragraph',          'label' => __( 'Paragraph' ,        'menu-item-types') );
+$menu_item_types[ 'image' ]             = array( 'slug' => 'image',              'label' => __( 'Image' ,            'menu-item-types') );
 $menu_item_types[ 'wpblock' ]           = array( 'slug' => 'wpblock',            'label' => __( 'Reusable Block',    'menu-item-types') );
 
 $menu_item_types[ 'post_type_archive' ] = array( 'slug' => 'post_type_archive',  'label' => __( 'Post Type Archive', 'menu-item-types') );
